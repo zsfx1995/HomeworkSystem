b14431d5548ba82ba8643c0d41c12a5d039232fa
zsfx1995 <1143831200@qq.com>
2017-03-16 21:22:30 +0800

    完成基本做题流程需要的接口

---
+++ service/exam/submitPaper.php
@@ -0,0 +1,51 @@
+<?php
+	
+	include_once("../../common/include/common.inc");
+	include_once("../../common/class/baseDatabase.inc");
+	include_once("../../common/class/baseTable.inc");
+	include_once("../../common/class/user.inc");
+	include_once("../../common/class/error.inc");
+	include_once("../../common/class/paper_finished.inc");
+	
+	if( isset($_POST["Uid"]) )
+		$Uid = (int) $_POST['Uid'];
+	else 
+		$Uid = 0;
+	if( isset($_POST["Pid"]) )
+		$Pid = (int) $_POST['Pid'];
+	else 
+		$Pid = 0;
+	
+	$paperFinishedObj = new paper_finished();
+	
+	if(!$paperFinishedObj->createConnect()){
+		$errorObj=$paperFinishedObj->getError();
+		$errorObj->showErrors($show_sql_flag=false);
+	}
+	
+	$paperFinishedObj -> s_Search( $Uid , $Pid );
+	$count = $paperFinishedObj -> getRecordCount();
+	
+	$paperFinishedObj -> setUid( isset($_POST["Uid"]) ?  (int) $_POST["Uid"] : 0 );
+	$paperFinishedObj -> setPid( isset($_POST["Pid"]) ?  (int) $_POST["Pid"] : 0 );
+	$paperFinishedObj -> setAllCount( isset($_POST["AllCount"]) ?  (int) $_POST["AllCount"] : 0 );
+	$paperFinishedObj -> setFinishedCount( isset($_POST["FinishedCount"]) ?  (int) $_POST["FinishedCount"] : 0 );
+	$paperFinishedObj -> setRightCount( isset($_POST["RightCount"]) ?  (int) $_POST["RightCount"] : 0 );
+	$paperFinishedObj -> setTimeUsed( isset($_POST["TimeUsed"]) ?  (int) $_POST["TimeUsed"] : 0 );
+	$paperFinishedObj -> setCondition( "Uid" , $Uid );
+	$paperFinishedObj -> setCondition( "Pid" , $Pid );
+	/*
+		数据存在则更新
+		数据不存在则插入
+	*/
+	
+	$flag = $count > 0 ? $paperFinishedObj -> updateRecorded() : $paperFinishedObj -> addRecorded() ;
+	if(!$flag){
+		$errorObj=$paperFinishedObj->getError();
+		$errorObj->showErrors($show_sql_flag=false);
+	}
+	$return_arr = array(
+		'ResCode' => $flag ? 101 : 201
+	);  
+	echo  json_encode($return_arr);
+?>
---
+++ service/questionList/add2WrongList.php
@@ -0,0 +1,42 @@
+<?php
+	
+	include_once("../../common/include/common.inc");
+	include_once("../../common/class/baseDatabase.inc");
+	include_once("../../common/class/baseTable.inc");
+	include_once("../../common/class/user.inc");
+	include_once("../../common/class/error.inc");
+	include_once("../../common/class/question_wrong.inc");
+	
+
+	$Uid = isset($_POST["Uid"]) ? (int) $_POST['Uid'] : 0;
+	$Qid = isset($_POST["Qid"]) ? (int) $_POST['Qid'] : 0;
+	
+	$question_wrongObj = new question_wrong();
+	
+	if(!$question_wrongObj->createConnect()){
+		$errorObj=$question_wrongObj->getError();
+		$errorObj->showErrors($show_sql_flag=false);
+	}
+	
+	$question_wrongObj -> s_Search( $Uid , $Qid );
+	$count = $question_wrongObj -> getRecordCount();
+	
+	$question_wrongObj -> setUid( $Uid );
+	$question_wrongObj -> setQid( $Qid );
+	
+	$question_wrongObj -> setCondition( "Uid" , $Uid );
+	$question_wrongObj -> setCondition( "Qid" , $Qid );
+	/*
+		²åÈë´íÌâ±í
+	*/
+	$flag = $count > 0 ? 1 : $question_wrongObj -> addRecorded() ;
+	if(!$flag){
+		$errorObj=$question_wrongObj->getError();
+		$errorObj->showErrors($show_sql_flag=false);
+	}
+	$return_arr = array(
+		'ResCode' => $flag ? 101 : 201
+	); 
+
+	echo  json_encode($return_arr);
+?>
